7c557b2a60829861b9298975725d52a528ee8af6
wesley <wesleyrl.21@gmail.com>
2022-05-20 00:45:13 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioArquivos/Produtos/Produto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ExercicioArquivos.Produtos
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void CreateArquivo(List<Produto> prod)
+        {
+            
+
+            try
+            {
+                var arquivo = @"c:\temp\Produtos\produtos.txt";
+
+                StreamWriter sw = new StreamWriter(arquivo);
+
+                sw.WriteLine("############ PRODUTOS #################");
+                foreach (var prods in prod)
+                {
+                    sw.WriteLine("Nome: "+ prods.Nome);
+                    sw.WriteLine("Preço: " + prods.Preco);
+                    sw.WriteLine("Quantidade: " + prods.Quantidade);
+                    sw.WriteLine("----------------------------");
+                }
+               
+               
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public void GerarCopy()
+        {
+            string arquivoOrigem = @"c:\temp\Produtos\produtos.txt"; 
+            string arquivoDestino = @"c:\temp\Produtos\ProdutosBackup\produtos.txt";
+
+            try
+            {
+                string[] linhas = File.ReadAllLines(arquivoOrigem);
+                using (StreamWriter sw = File.AppendText(arquivoDestino))
+                {
+                    foreach (string line in linhas)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+
+                Console.WriteLine("Copia gerada com sucesso!");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Um erro ocorreu! " + e.Message);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Nome {Nome} \n" +
+                   $"Preço: {Preco} \n" +
+                   $"Quantidade: {Quantidade}";
+        }
+
+
+
+
+    }
+}
---
+++ ExercicioArquivos/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercicioArquivos.Produtos;
+
+namespace ExercicioArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 0;
+            Produto produto = new Produto();
+            List<Produto> ListProduto = new List<Produto>();
+
+            do
+            {
+                string msg = "Digite 0 para sair \n Digite 1 para salvar \ndigite 2 para realizar o backup da lista \nou qual quer outro  numero para adicionar mais produtos";
+                switch (i)
+                {
+                    case 1:
+
+                        produto.CreateArquivo(ListProduto);
+                        Console.WriteLine(msg);
+                        break;
+                    case 2:
+
+                        produto.GerarCopy();
+                        Console.WriteLine(msg);
+                        break;
+                    default:
+                        Console.WriteLine("Adicione os items a sua lista de produtos:");
+
+                        Console.WriteLine("Informe o nome do produto: ");
+                        string nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe o preço do produto:");
+                        double preco = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Informe a quantidade do produto:");
+                        int qtd = int.Parse(Console.ReadLine());
+
+                        ListProduto.Add(new Produto(nome, preco, qtd));
+
+
+
+                        Console.WriteLine(msg);
+
+                        break;
+                }
+                
+                i = int.Parse(Console.ReadLine());
+            } while (i != 0);
+
+            
+            
+        }
+    }
+}
